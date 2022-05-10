41d1914bbf250c74d8d7f5637e1dc349cf2f1c91
Angapparaja <88724733+Angapparaja@users.noreply.github.com>
2022-05-10 13:05:34 +0530

    updated new code

--- src/main/java/com/ba/Task/factory/DriverFactory.java
+++ src/main/java/com/ba/Task/factory/DriverFactory.java
@@ -44,8 +44,9 @@ public class DriverFactory {
 			
 			
 			if(browserName.equalsIgnoreCase("chrome")) {
-				WebDriverManager.chromedriver().setup();
-				
+				//WebDriverManager.chromedriver().setup();
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\angap\\Downloads\\New folder (3)\\chromedriver.exe");
+				  
 				if(Boolean.parseBoolean(prop.getProperty("remote"))){
 					init_remoteDriver("chrome");
 				}
